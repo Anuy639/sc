fb529e26a2d0c2006b05bd3ff93504f206106dc0
Anuy639 <118895368+Anuy639@users.noreply.github.com>
2022-12-23 21:22:49 +0800

    Update addssr.sh

--- ssr/addssr.sh
+++ ssr/addssr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# My Telegram : https://t.me/anuybazoelk
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -90,4 +90,4 @@ echo -e "Expired     : ${exp} "
 echo -e "=========================="
 echo -e "Link SSR    : ${ssr_link}"
 echo -e "=========================="
-echo -e "Script By RIDWAN STORE"
+echo -e "Script By Nyari Gratisan"
