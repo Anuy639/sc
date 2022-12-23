9222d70e1bce4671f6c789be03ebadded01d1f60
Anuy639 <118895368+Anuy639@users.noreply.github.com>
2022-12-23 21:24:09 +0800

    Update renewssr.sh

--- ssr/renewssr.sh
+++ ssr/renewssr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# My Telegram : https://t.me/anuybazoelk
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -70,4 +70,4 @@ echo "======================="
 echo "Username  : $user"
 echo "Expired   : $exp4"
 echo "======================="
-echo "Script By RIDWAN STORE"
+echo "Script By Nyari Gratisan"
