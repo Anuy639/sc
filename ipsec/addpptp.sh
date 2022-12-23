d5181d19728c8b7506f55bfe8d25da1dd40c93c2
Anuy639 <118895368+Anuy639@users.noreply.github.com>
2022-12-23 20:17:33 +0800

    Update addpptp.sh

--- ipsec/addpptp.sh
+++ ipsec/addpptp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# My Telegram : https://t.me/anuybazoelk
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -76,5 +76,5 @@ Password  : $VPN_PASSWORD
 Created   : $hariini
 Expired   : $exp
 ============================
-Script By RIDWAN STORE
+Script By NyariGratisan
 EOF
