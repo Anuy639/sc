a9675c5a1dbf5708eca62c44c6417fa2a76bc617
Anuy639 <118895368+Anuy639@users.noreply.github.com>
2022-12-26 11:03:15 +0800

    Update trialssh.sh

--- ssh/trialssh.sh
+++ ssh/trialssh.sh
@@ -82,7 +82,7 @@ echo -e "❇️ BadVpn        : 7100-7200-7300"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m          ⇱ Payload Websocket Tls ⇲          \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "GET ws://${domain} HTTP/1.1\r
+echo -e "HEAD ws://${domain} HTTP/1.1\r
 Host: bug.com\r
 Connection: keep-alive\r
 Proxy-Connection: keep-alive\r
